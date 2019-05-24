7f32c0395a43ea9cb2cdc06578c3cf6120d2aaf1
supunsandeeptha <sandeeptha.supun@gmail.com>
2019-05-24 23:39:58 +0530

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+ name:any;
+ showResponse;
 
+  constructor(private HttpClient : HttpClient) {} //injecting the http client to the constructor
+
+send(){
+
+  let sendName = {
+    name:this.name
+  }
+  
+  this.HttpClient.post('http://localhost:9000/send',sendName).subscribe(
+    response =>{
+        console.log(response); //logging the response
+        let x = response;
+        this.showResponse = response;
+        alert(x);
+
+
+    }
+  )
+
+
+}
+
+ 
 }
